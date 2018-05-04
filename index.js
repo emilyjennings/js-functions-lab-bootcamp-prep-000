17fc920b2bb48bdfd3df91c02977a680b5295906
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-04 20:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function happyHolidayTo(holiday, name ) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
