8d3c7e9cc3f9412c1b38e31766b365bdd6dfbcad
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-04 19:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  name = "you";
+  return "Happy holidays, ${name}!";
+}
+
+function happyHolidayTo(holiday, name) {
+  holiday = "Christmas";
+  name = "James";
+  return "Happy ${holiday}, ${name}!";
+}
+
+function holidayCountdown("Passover", 3) {
+  return `It's ${days} days until ${holiday}!`
+}
